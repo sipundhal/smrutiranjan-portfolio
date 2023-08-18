c35cea8b666c1f61d5197dd07bf87f19ee1280c2
Smruti-Ranjan-Divi <smruti@divii.com>
2023-08-18 18:21:33 +0530

    Update experienceData.js

--- src/data/experienceData.js
+++ src/data/experienceData.js
@@ -1,45 +1,52 @@
 export const experienceData = [
-    
-    {
-        id: 1,
-        company: 'Syon India Advance Solution',
-        jobtitle: 'Node Js Developer(Internship)',
-        startYear: '2018-may',
-        endYear: '2018-july'
-    },
-    {
-        id: 2,
-        company: 'Appstone Pvt Ltd',
-        jobtitle: 'Web Developer(Internship)',
-        startYear: '2019-january',
-        endYear: '2019-april'
-    },
-    {
-        id: 3,
-        company: 'Iserveu Technology',
-        jobtitle: 'Software Developer',
-        startYear: '2020-september',
-        endYear: '2020-November'
-    },
-    {
-        id: 4,
-        company: 'Codekart Technology',
-        jobtitle: 'Web Developer',
-        startYear: '2021-June',
-        endYear: '2021-September'
-    },
-    {
-        id: 5,
-        company: 'JeevaOrganic Pvt Ltd',
-        jobtitle: 'Associate Software Engineer',
-        startYear: '2021-September',
-        endYear: '2021-December'
-    },
-    {
-        id: 6,
-        company: 'Hanriver Technology',
-        jobtitle: 'Frontend Developer',
-        startYear: '2022-February',
-        endYear: 'Continue'
-    },
-]
+  {
+    id: 1,
+    company: "Syon India Advance Solution",
+    jobtitle: "Node Js Developer(Internship)",
+    startYear: "2018-may",
+    endYear: "2018-july",
+  },
+  {
+    id: 2,
+    company: "Appstone Pvt Ltd",
+    jobtitle: "Web Developer(Internship)",
+    startYear: "2019-january",
+    endYear: "2019-april",
+  },
+
+  {
+    id: 3,
+    company: "Codekart Technology",
+    jobtitle: "Web Developer",
+    startYear: "2019-June",
+    endYear: "2019-August",
+  },
+  {
+    id: 4,
+    company: "Iserveu Technology",
+    jobtitle: "Software Developer",
+    startYear: "2020-April",
+    endYear: "2020-July",
+  },
+  {
+    id: 5,
+    company: "Zuesso Business Solutions Pvt. Ltd.",
+    jobtitle: "Vue Js Developer",
+    startYear: "2020-August",
+    endYear: "2021-August",
+  },
+  {
+    id: 6,
+    company: "JeevaOrganic Pvt Ltd",
+    jobtitle: "Associate Software Engineer",
+    startYear: "2021-September",
+    endYear: "2021-December",
+  },
+  {
+    id: 7,
+    company: "Hanriver Technology",
+    jobtitle: "Frontend Developer",
+    startYear: "2022-February",
+    endYear: "2023-July",
+  },
+];
